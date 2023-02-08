07370701a14dff1477e32c03f63f15f71fac552a
michellevu4 <michelle.vu@ufl.edu>
2023-02-08 13:34:48 -0500

    try different authentication methods

--- api/main.go
+++ api/main.go
@@ -22,6 +22,10 @@ func main() {
 
 	client := spotify.Authenticator{}.NewClient(accessToken)
 
+	// try to use search function
+	results, err := client.Search("holiday", spotify.SearchTypePlaylist|spotify.SearchTypeAlbum)
+	log.Println("Results:", results)
+
 	playlistID := spotify.ID("37i9dQZF1DXcBWIGoYBM5M")
 	playlist, err := client.GetPlaylist(playlistID)
 	if err != nil {
@@ -32,3 +36,128 @@ func main() {
 	log.Println("playlist name:", playlist.Name)
 	log.Println("playlist description:", playlist.Description)
 }
+
+// This example demonstrates how to authenticate with Spotify using the authorization code flow.
+// In order to run this example yourself, you'll need to:
+//
+//  1. Register an application at: https://developer.spotify.com/my-applications/
+//     - Use "http://localhost:8080/callback" as the redirect URI
+//  2. Set the SPOTIFY_ID environment variable to the client ID you got in step 1.
+//  3. Set the SPOTIFY_SECRET environment variable to the client secret from step 1.
+
+/*package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/zmb3/spotify/v2"
+	"github.com/zmb3/spotify/v2/auth"
+)
+
+// redirectURI is the OAuth redirect URI for the application.
+// You must register an application at Spotify's developer portal
+// and enter this value.
+const redirectURI = "http://localhost:8080/callback"
+
+var (
+	auth  = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate))
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
+)
+
+func main() {
+	// first start an HTTP server
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+	// wait for auth to complete
+	client := <-ch
+
+	// use the client to make calls that require authorization
+	user, err := client.CurrentUser(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("You are logged in as:", user.ID)
+}
+
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(r.Context(), state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+
+	// use the token to get an authenticated client
+	client := spotify.New(auth.Client(r.Context(), tok))
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- client
+}*/
+
+/*package main
+
+import (
+	"context"
+	"fmt"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"log"
+	"os"
+
+	"golang.org/x/oauth2/clientcredentials"
+
+	"github.com/zmb3/spotify/v2"
+)
+
+func main() {
+	ctx := context.Background()
+	config := &clientcredentials.Config{
+		ClientID:     os.Getenv("SPOTIFY_ID"),
+		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
+		TokenURL:     spotifyauth.TokenURL,
+	}
+	token, err := config.Token(ctx)
+	if err != nil {
+		log.Fatalf("couldn't get token: %v", err)
+	}
+
+	httpClient := spotifyauth.New().Client(ctx, token)
+	client := spotify.New(httpClient)
+	// search for playlists and albums containing "holiday"
+	results, err := client.Search(ctx, "holiday", spotify.SearchTypePlaylist|spotify.SearchTypeAlbum)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// handle album results
+	if results.Albums != nil {
+		fmt.Println("Albums:")
+		for _, item := range results.Albums.Albums {
+			fmt.Println("   ", item.Name)
+		}
+	}
+	// handle playlist results
+	if results.Playlists != nil {
+		fmt.Println("Playlists:")
+		for _, item := range results.Playlists.Playlists {
+			fmt.Println("   ", item.Name)
+		}
+	}
+}*/

704716904e4f403319833f69fe6216713b92e08e
VallinEMCSQ <vallinc@ufl.edu>
2023-03-01 13:03:35 -0500

    Basic GUI for initial login and join page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { PlayerComponent } from './components/player/player.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerComponent
+    PlayerComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

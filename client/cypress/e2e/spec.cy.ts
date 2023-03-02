a9b06475b07d9028e21f03aa8e79015e82be600c
Teresa Vu <95201928+TeresaVu@users.noreply.github.com>
2023-03-02 01:58:47 -0500

    Update spec.cy.ts

--- client/cypress/e2e/spec.cy.ts
+++ client/cypress/e2e/spec.cy.ts
@@ -2,7 +2,7 @@ describe('My First Test', () => {
   it('Clicks the join button which opens spotify web ', () => {
     cy.visit('http://localhost:4200')
 
-    cy.contains("Login to Spotify to host a session")
+    cy.contains("Login to Spotify to host a session").click()
     // cy.url().should('include', 'spotify.com')
   })
 })

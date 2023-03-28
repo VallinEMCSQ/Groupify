2bef84fd0d3c827a52aa9a92907c39609dd5efb7
TeresaVu <95201928+TeresaVu@users.noreply.github.com>
2023-03-27 21:49:47 -0400

    post functions

--- client/src/app/pages/start/start.component.ts
+++ client/src/app/pages/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { StartService } from './start.service';
 
 @Component({
@@ -7,11 +8,27 @@ import { StartService } from './start.service';
   styleUrls: ['./start.component.css']
 })
 export class StartComponent implements OnInit{
+  code!: string;
+  state!: string;
   authToken: any;
 
-  constructor(private startService: StartService) {}
+  constructor(private startService: StartService, private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      this.code = params['param1'];
+      this.state = params['param2'];
+    });
+    console.log("Code: ", this.code);
+    console.log("State: ", this.state);
+  }
 
   ngOnInit(): void {
+    // send a post to backend containing the code and state from the url query parameters
+    this.startService.sendAuthParams(this.code, this.state).subscribe(
+      (response: any) => {
+        this.authToken = response.link
+        console.log("Token received and stored: ", this.authToken)
+      }
+    )
     // complete spotify authorization and receive access token
     this.startService.getToken().subscribe(
       response => {

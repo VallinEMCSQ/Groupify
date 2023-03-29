20ce6ab006086ba8bec8897956b42d93595c692d
TeresaVu <95201928+TeresaVu@users.noreply.github.com>
2023-03-29 12:18:47 -0400

    create session and get/store session PIN

--- client/src/app/pages/start/start.service.ts
+++ client/src/app/pages/start/start.service.ts
@@ -10,18 +10,18 @@ export class StartService {
 
   constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
-  sendAuthParams(code: string, state: string) {
-    return this.http.post('http://localhost:8080/callback', code)
-  }
-
   // backend completes spotify authorization and sends access token
-  getToken(): Observable<any> {
+  getToken(code: string, state: string): Observable<any> {
     console.log("start.service: Getting token through http request")
-    return this.http.get('http://localhost:8080/token', {
+    return this.http.get('http://localhost:8080/callback', {
       params: {
-        code: 'code',
-        state: 'state'
+        code: code,
+        state: state
       }
     })
+  }
+  // backend creates a new session PIN, stores it in the db, and returns the PIN
+  createSession(): Observable<any> {
+    return this.http.get('http://localhost:8080/create-session')
   }
 }

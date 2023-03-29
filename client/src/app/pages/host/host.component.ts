14bd60e0ea5850fe59ec7103d27903220da98351
TeresaVu <95201928+TeresaVu@users.noreply.github.com>
2023-03-29 09:07:50 -0400

    add host page

---
+++ client/src/app/pages/host/host.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-host',
+  templateUrl: './host.component.html',
+  styleUrls: ['./host.component.css']
+})
+export class HostComponent {
+
+}

670fc8a6252f7529c10cb7ed1705b86bfca24654
VallinEMCSQ <vallinc@ufl.edu>
2023-03-29 16:05:05 -0400

    Trying something

---
+++ client/src/app/animations.ts
@@ -0,0 +1,30 @@
+import{
+    trigger,
+    state,
+    style,
+    animate,
+    transition,
+    // ...
+  
+} from '@angular/animations'
+
+export let fadeIn = trigger('fadeIn', [
+    transition('void => *', [
+      style({ opacity: 0}),
+      animate(2000)
+    ])
+]);
+
+export let expandUp = trigger('expandUp', [
+    transition('void => *', [
+      style({ height: '15px'}),
+      animate(2000)
+    ])
+]);
+
+export let expandWidth = trigger('expandWidth', [
+    transition('void => *', [
+      style({ width: '0px'}),
+      animate(2000)
+    ])
+]);
---
+++ client/src/app/pages/start/start.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { expandUp, expandWidth, fadeIn } from 'src/app/animations';
+import { StartService } from './start.service';
+
+@Component({
+  selector: 'app-start',
+  templateUrl: './start.component.html',
+  styleUrls: ['./start.component.css'],
+  animations: [
+    fadeIn,
+    expandUp,
+    expandWidth
+  ]
+})
+export class StartComponent implements OnInit{
+  authToken: any;
+
+  constructor(private startService: StartService) {}
+
+  ngOnInit(): void {
+    // complete spotify authorization and receive access token
+    this.startService.getToken().subscribe(
+      response => {
+        this.authToken = response.link
+        console.log("Token received and stored: ", this.authToken)
+      }
+    )
+  }
+
+}
